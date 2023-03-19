64b4d2ab7081d7610fdbc24ca117269ccdcea9d9
Khoirunnisa' Rizky Noor Fatimah <49296015+khoirunnisarizky@users.noreply.github.com>
2023-03-19 22:21:29 +0700

    Initial Commit

---
+++ MyAvatar/MyAvatarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAvatarApp.swift
+//  MyAvatar
+//
+//  Created by khoirunnisa' rizky noor fatimah on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MyAvatarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
